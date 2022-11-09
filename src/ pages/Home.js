1ae85135717761c592522f62bd2f999a61023b4a
Lucas Reis Oliveira Vargas Lomiento <lucaslomiento@icloud.com>
2022-11-09 18:39:54 -0300

    retirada de duplicata de import da api

--- src/ pages/Home.js
+++ src/ pages/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { getProductsFromCategoryAndQuery } from '../services/api';
 import * as api from '../services/api';
 
 export default class Home extends Component {
